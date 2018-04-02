8044de9bbfd53514b495813b7218b88a5b6636fc
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-02 11:55:37 -0400

    Add code challenge

---
+++ bool_to_word.php
@@ -0,0 +1,4 @@
+<?php
+//  8 kyu - Convert boolean values to strings 'Yes' or 'No'.
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. -->
+?>
