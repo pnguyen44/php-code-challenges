7baf0ac27a661933129b54a1b45f9d2852c4da8c
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-02 11:54:40 -0400

    Clean code

--- reversed_strings.php
+++ reversed_strings.php
@@ -1,9 +1,8 @@
-<!-- 7 kyu - Reversed Strings
-Complete the solution so that it reverses the string value passed into it.
+<?php
+// 7 kyu - Reversed Strings
+// Complete the solution so that it reverses the string value passed into it.
 
 solution("world"); // returns "dlrow" -->
-
-<?php
   function solution($str) {
     return strrev($str);
   };
