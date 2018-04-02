29a5f22e292677f6f0eeedd9df3053194f0b160f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-02 13:08:48 -0400

    Add cube_odd function  - A function that find the sum of the odd numbers within an array,  after cubing the initial integers

--- cube_odd.php
+++ cube_odd.php
@@ -2,4 +2,24 @@
 // 7 kyu - Sum of Odd Cubed Numbers
 // Find the sum of the odd numbers within an array, after cubing the initial integers. This function will return undefined (NULL in PHP) if any of the values aren't numbers.
 
+function cube_odd($a) {
+  $sum = 0;
+  $num_only = array_filter($a, function($n) {
+    return is_numeric($n);
+  });
+  if (count($num_only) === count($a)) {
+    foreach($a as $num) {
+      if (abs($num) % 2 > 0) {
+        $sum +=$num ** 3;
+      };
+    };
+    return $sum;
+  } else {
+    return null;
+  }
+};
+
+$answer =  cube_odd(["a", 12, 9, "z", 42]);
+print_r("$answer \n");
+
 ?>
