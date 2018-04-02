280a292b141e7b4cf716538c4322cf5ec87692ba
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-02 11:32:54 -0400

    Add code challenge

---
+++ in_asc_order.php
@@ -0,0 +1,13 @@
+<!-- 7 kyu- Are the numbers in order?
+
+Are the numbers in order?
+In this Kata, your function receives an array of positive integers as input. Your task is to determine whether the numbers are in ascending order.
+
+For the purposes of this Kata, you may assume that all inputs are valid (i.e. arrays containing only positive integers with a length of at least 2).
+
+For example:
+
+in_asc_order([1, 2, 4, 7, 19]); // true
+in_asc_order([1, 2, 3, 4, 5]); // true
+in_asc_order([1, 6, 10, 18, 2, 4, 20]); // false
+in_asc_order([9, 8, 7, 6, 5, 4, 3, 2, 1]); // false (NOTE: because the numbers are in DESCENDING order, not ascending order) -->
