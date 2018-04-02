065894e2c48df5ce730c3140dd9e58f92df671b5
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-02 13:56:41 -0400

    Add find_average function  - A function that calculates average of numbers in given list.

--- find_average.php
+++ find_average.php
@@ -2,4 +2,7 @@
 // 8 kyu - Calculate average
 // Write function avg which calculates average of numbers in given list.
 
+function find_average($array) {
+  return array_sum($array)/count($array);
+}
 ?>
