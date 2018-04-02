a913d965926ce0a3bee79f04effd79113d5ea32d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-02 11:59:09 -0400

    Refractor code

--- bool_to_word.php
+++ bool_to_word.php
@@ -3,11 +3,7 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. -->
 
 function boolToWord($bool){
-  if($bool === true) {
-    return 'Yes';
-  } else {
-    return 'No';
-  };
+  return $bool === true ? "Yes" : "No";
 };
 
 $answer = boolToword(true);
