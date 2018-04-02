d9c5e37e51f718f3cd991a61e535f1f9a4538c5a
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-02 11:53:20 -0400

    Clean code

--- in_asc_order.php
+++ in_asc_order.php
@@ -1,19 +1,19 @@
-<!-- 7 kyu- Are the numbers in order?
-
-Are the numbers in order?
-In this Kata, your function receives an array of positive integers as input. Your task is to determine whether the numbers are in ascending order.
-
-For the purposes of this Kata, you may assume that all inputs are valid (i.e. arrays containing only positive integers with a length of at least 2).
-
-For example:
-
-in_asc_order([1, 2, 4, 7, 19]); // true
-in_asc_order([1, 2, 3, 4, 5]); // true
-in_asc_order([1, 6, 10, 18, 2, 4, 20]); // false
-in_asc_order([9, 8, 7, 6, 5, 4, 3, 2, 1]); // false (NOTE: because the numbers are in DESCENDING order, not ascending order) -->
-
-
 <?php
+// 7 kyu- Are the numbers in order?
+//
+// Are the numbers in order?
+// In this Kata, your function receives an array of positive integers as input. Your task is to determine whether the numbers are in ascending order.
+//
+// For the purposes of this Kata, you may assume that all inputs are valid (i.e. arrays containing only positive integers with a length of at least 2).
+//
+// For example:
+//
+// in_asc_order([1, 2, 4, 7, 19]); // true
+// in_asc_order([1, 2, 3, 4, 5]); // true
+// in_asc_order([1, 6, 10, 18, 2, 4, 20]); // false
+// in_asc_order([9, 8, 7, 6, 5, 4, 3, 2, 1]); // false (NOTE: because the numbers are in DESCENDING order, not ascending order) -->
+
+
 function in_asc_order($arr) {
   $arr1 = $arr;
   $x = sort($arr);
@@ -33,4 +33,7 @@ function in_asc_order($arr) {
 //   return true;
 //
 // };
+
+$answer = in_asc_order([1,2,3]);
+print_r("$answer \n");
 ?>
