7f1a1df8f224a7d88870ff9f3957dbe70477fcf9
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-02 14:43:12 -0400

    Add fake_bin function   -A function that replace any digit below 5 with '0' and any digit 5   and above with '1' and return the resulting string.

--- fake_bin.php
+++ fake_bin.php
@@ -1,4 +1,20 @@
 <?php
 // 8 kyu - Fake Binary
 // Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+function fake_bin($s) {
+  $new = '';
+  for($i = 0; $i < strlen($s); $i++) {
+    if (intval($s[$i]) < 5) {
+      $new = $new . '0';
+    } else {
+      $new = $new . '1';
+    };
+  };
+  return $new;
+};
+
+$answer =  fake_bin('45385593107843568');
+print_r("$answer \n");
+
 ?>
