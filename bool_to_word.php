112d960823ab22a2f2bc412f881352f8cc2766d6
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-02 11:57:25 -0400

    Add boolToWord function   -A function that convert boolean values to strings 'Yes' or 'No'

--- bool_to_word.php
+++ bool_to_word.php
@@ -1,4 +1,15 @@
 <?php
 //  8 kyu - Convert boolean values to strings 'Yes' or 'No'.
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false. -->
+
+function boolToWord($bool){
+  if($bool === true) {
+    return 'Yes';
+  } else {
+    return 'No';
+  };
+};
+
+$answer = boolToword(true);
+print_r("$answer \n");
 ?>
