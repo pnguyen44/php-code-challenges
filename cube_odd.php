4e81a3c858963bd0933a551f3d75d542a746e6ef
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-02 13:07:47 -0400

    Add code challenge

---
+++ cube_odd.php
@@ -0,0 +1,5 @@
+<?php
+// 7 kyu - Sum of Odd Cubed Numbers
+// Find the sum of the odd numbers within an array, after cubing the initial integers. This function will return undefined (NULL in PHP) if any of the values aren't numbers.
+
+?>
