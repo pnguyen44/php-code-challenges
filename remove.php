43e0a0f13a24a9a96e2313d23d1426f7535f49a4
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-03 14:52:30 -0400

    Add code challenge

---
+++ remove.php
@@ -0,0 +1,15 @@
+<?php
+// 8 kyu - Exclamation marks series #2: Remove all exclamation marks from the end of sentence
+// Description:
+// Remove all exclamation marks from the end of sentence.
+//
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+// Note
+// Please don't post issue about difficulty or duplicate.
+?>
