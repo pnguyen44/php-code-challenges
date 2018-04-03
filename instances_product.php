724dfb116216525f9e7838134cb050ef670335c2
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-03 15:17:24 -0400

    Add code challenge

---
+++ instances_product.php
@@ -0,0 +1,19 @@
+<?php
+// 7 kyu - Exclamation marks series #13: Count the number of exclamation marks and question marks, return the product
+// Description:
+// Count the number of exclamation marks and question marks, return the product.
+//
+// Examples
+// Product("") == 0
+// product("!") == 0
+// Product("!ab? ?") == 2
+// Product("!!") == 0
+// Product("!??") == 2
+// Product("!???") == 3
+// Product("!!!??") == 6
+// Product("!!!???") == 9
+// Product("!???!!") == 9
+// Product("!????!!!?") == 20
+
+
+?>
