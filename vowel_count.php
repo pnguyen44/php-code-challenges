aaec864821dd72f52c5a8ac5853126371de07d4d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-03 15:32:46 -0400

    Add code challenge

---
+++ vowel_count.php
@@ -0,0 +1,8 @@
+<?php
+// 7 kyu - Vowel Count
+// Return the number (count) of vowels in the given string.
+//
+// We will consider a, e, i, o, and u as vowels for this Kata.
+//
+// The input string will only consist of lower case letters and/or spaces.
+?>
