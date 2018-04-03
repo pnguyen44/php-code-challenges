ccc72e0f46c32b7ab8b9480edc337f10314a9aac
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-03 15:33:33 -0400

    Add getCount function   - A function that return the number (count) of vowels in the given   string.

--- vowel_count.php
+++ vowel_count.php
@@ -5,4 +5,10 @@
 // We will consider a, e, i, o, and u as vowels for this Kata.
 //
 // The input string will only consist of lower case letters and/or spaces.
+
+function getCount($str) {
+  $search = array('a','e','i','o','u');
+  return  abs(strlen($str) - strlen(str_replace($search, '', $str)));
+}
+
 ?>
