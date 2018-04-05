e23cd941177f0ca8a1f785018b7401080a0760cc
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-05 16:14:58 -0400

    Add code challenge

---
+++ find_missing_letter.php
@@ -0,0 +1,15 @@
+<?php
+// 6 kyu - Find the missing letter
+// #Find the missing letter
+//
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+//
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+//
+// Example:
+//
+// ['a','b','c','d','f'] -> 'e'
+// ['O','Q','R','S'] -> 'P'
+// (Use the English alphabet with 26 letters!)
+?>
