6220cbbcd80acddaef0648df3a1fc9e85afcdd5a
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-09 15:53:27 -0400

    Add anagrams function   - A function that will find all the anagrams of a word from a list.

--- anagrams.php
+++ anagrams.php
@@ -14,4 +14,24 @@
 // anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']); // => ['carer', 'racer']
 //
 // anagrams('laser', ['lazing', 'lazy',  'lacer']); // => []
+
+function anagrams(string $word, array $words): array {
+    $a = str_split($word);
+    sort($a);
+    $match = implode('', $a);
+    $result = [];
+    foreach($words as $w) {
+        $a = str_split($w);
+        sort($a);
+        $test = implode('',$a);
+        if ($test === $match) {
+          $result[] = $w ;
+        }
+    }
+    return $result;
+}
+
+$answer =  anagrams('racer', ['carer', 'arcre', 'carre', 'racrs', 'racers', 'arceer', 'raccer', 'carrer', 'cerarr']);
+print_r("$answer \n");
+
 ?>
