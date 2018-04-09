91a4802ab9b9b2f54356a0baa7df5b9a40318765
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-09 18:07:18 -0400

    Add code challenge

---
+++ get_length_of_missing_array.php
@@ -0,0 +1,18 @@
+<?php
+// 6 kyu - Length of missing array
+// You get an array of arrays.
+// If you sort the arrays by their length, you will see, that their length-values are consecutive.
+// But one array is missing!
+//
+//
+// You have to write a method, that return the length of the missing array.
+//
+// Example:
+// [[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+//
+//
+// If the array of arrays is null/nil or empty, the method should return 0.
+//
+// When an array in the array is null or empty, the method should return 0 too!
+// There will always be a missing element and its length will be always between the given arrays.
+?>
