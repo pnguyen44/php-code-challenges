42793d38342840c7d25b3e68d2de999e1b68d361
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-10 15:36:13 -0400

    Add code challenge

---
+++ string_transformer.php
@@ -0,0 +1,12 @@
+<?php
+// 6 kyu - String transformer
+// Given a string, return a new string that has transformed based on the input:
+//
+// Change case of every character, ie. lower case to upper case, upper case to lower case.
+// Reverse the order of words from the input.
+// For example:
+// stringTransformer('Example Input')/string_transformer("Example Input") (depending on the language you are completing the Kata in) should return 'iNPUT eXAMPLE'
+//
+// You may assume the input only contain English alphabet and spaces.
+
+?>
