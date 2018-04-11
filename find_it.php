17fcba705e33749e27bb375a4e7100d70276da03
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-11 16:36:23 -0400

    Add findIt function   - A function that takes an array and find the int that appears an odd   number of times.

--- find_it.php
+++ find_it.php
@@ -4,5 +4,14 @@
 //
 // There will always be only one integer that appears an odd number of times.
 
+function findIt(array $seq) : int {
+    $unique = array_unique($seq);
+    $result = array_filter(array_count_values($seq), function($v) {
+      return $v % 2 !== 0;
+    });
+    return array_keys($result)[0];
+  }
 
+$answer =  findIt([1,1,1,1,1,1,10,1,1,1,1]);
+print_r("$answer \n");
 ?>
