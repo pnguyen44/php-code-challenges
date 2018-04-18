a9256b7977b8b42a851d72483d27856e3f7efe5d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-17 23:25:54 -0400

    Add sum_of_integers_in_string function   - A function that calculates the sum of the integers inside a string.

--- sum_of_integers_in_string.php
+++ sum_of_integers_in_string.php
@@ -1,4 +1,12 @@
 <?php
 // 7 kyu - Sum of integers in string
 // Your task in this kata is to implement a function that calculates the sum of the integers inside a string. For example, in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
+
+function sum_of_integers_in_string(string $s): int {
+    $s = str_replace('.', ' ',$s);
+    return array_sum(explode(' ', preg_replace('/[a-z]+/i', ' ', $s)));
+}
+
+$answer = sum_of_integers_in_string("12.4");
+print_r("$answer \n");
 ?>
