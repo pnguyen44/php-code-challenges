1bdafe19d2ab64fd3d57099f0c445ec890e3043c
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-25 01:13:26 -0400

    Add code challenge

---
+++ split_strings.php
@@ -0,0 +1,8 @@
+<?php
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+//
+// Examples:
+//
+// solution('abc') // should return ['ab', 'c_']
+// solution('abcdef') // should return ['ab', 'cd', 'ef']
+?>
