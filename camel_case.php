0ca2d73a0529d360f3e27cf996f4b292e80c0d50
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-25 00:13:14 -0400

    Add code challenge

---
+++ camel_case.php
@@ -0,0 +1,9 @@
+<?php
+// 6 kyu - CamelCase Method
+// Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+//
+// For instance:
+//
+// camel_case("hello case"); // => "HelloCase"
+// camel_case("camel case word"); // => "CamelCaseWord"
+?>
