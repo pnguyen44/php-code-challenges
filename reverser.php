e9b04139856c54095e586c53f762005c06a14d8b
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-25 01:36:34 -0400

    Add code challenge

---
+++ reverser.php
@@ -0,0 +1,23 @@
+<?php
+// 7 kyu -  Reverse Letters in Sentence
+// Take a sentence (string) and reverse each letter in each word in the sentence. Do not reverse the order of the words, just the letters in each word.
+//
+// If there is punctuation, it should be interpreted as a regular character; no special rules.
+//
+// If there is spacing before/after the input string, leave them there.
+//
+// String will always be defined.
+//
+// Example:
+//
+// "A fun little challenge!" => 'A nuf elttil !egnellahc'
+//
+// " A fun little challenge! "=> ' A nuf elttil !egnellahc '
+//
+// Addendum: Your function receives a writeable null-terminated string.
+//
+// Mutate the string in-place and return the sentence.
+//
+// For this kata, you are not required to allocate or de-allocate any memory.
+
+?>
