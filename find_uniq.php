2a53e3dc2e5eb5e8a61a5ecd50139ec94636426f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-26 03:02:32 -0400

    Add code challenge

---
+++ find_uniq.php
@@ -0,0 +1,10 @@
+<?php
+// 6 kyu - Find the unique number
+// There is an array with some numbers. All numbers are equal except for one. Try to find it!
+//
+// findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+// findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+// It’s guaranteed that array contains more than 3 numbers.
+//
+// The tests contain some very huge arrays, so think about performance.
+?>
