190b8eeaf2773612eb8fb7baad6808a892a3f153
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-25 23:13:08 -0400

    Add code challenge

---
+++ longest.php
@@ -0,0 +1,8 @@
+<?php
+// 7 kyu - Thinkful - List Drills: Longest word
+// Complete the function that takes one argument, a list of words, and returns the length of the longest word in the list.
+//
+// For example:
+//
+// ['simple', 'is', 'better', 'than', 'complex'] ==> 7
+?>
