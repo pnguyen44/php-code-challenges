b6554bbc3c325ff7a9b1745dfd8d6241b4689dab
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-26 00:46:16 -0400

    Add alternative solution

--- dashatize.php
+++ dashatize.php
@@ -6,16 +6,22 @@
 //
 // dashatize(274) -> '2-7-4'
 // dashatize(6815) -> '68-1-5'
+
+// function dashatize(int $num): string {
+//   $arr = str_split($num);
+//   $result = '';
+//   foreach($arr as $val) {
+//     $result .= intval($val) % 2 === 0 ? $val : '-' . $val . '-';
+//   }
+//   return str_replace('--', '-', rtrim(ltrim($result,'-'), '-'));
+// }
+//
+// Alternative Solution:
 function dashatize(int $num): string {
-  $arr = str_split($num);
-  $result = '';
-  foreach($arr as $val) {
-    $result .= intval($val) % 2 === 0 ? $val : '-' . $val . '-';
-  }
-  return str_replace('--', '-', rtrim(ltrim($result,'-'), '-'));
+  return trim(str_replace('--', '-', preg_replace('/([13579])/', '-$1-', $num)),'-');
 }
 
-$answer = dashatize(18929);
+$answer = dashatize(6815);
 print_r("$answer \n");
 
 ?>
