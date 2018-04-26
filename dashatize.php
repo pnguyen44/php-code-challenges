2e54ad18dd0d6fcbdf4b40617d0976b97905b78c
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-26 00:27:22 -0400

    Add code challenge

---
+++ dashatize.php
@@ -0,0 +1,9 @@
+<?php
+// 6 kyu - Dashatize it
+// Given a number, return a string with dash'-'marks before and after each odd integer, but do not begin or end the string with a dash mark.
+//
+// Ex:
+//
+// dashatize(274) -> '2-7-4'
+// dashatize(6815) -> '68-1-5'
+?>
