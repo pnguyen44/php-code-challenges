6cd3283a50c1d718b93e343237c62320833fa22d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-27 03:22:39 -0400

    Add code challenge

---
+++ rotate_to_max.php
@@ -0,0 +1,9 @@
+<?php
+// 7 kyu - Rotate to the max
+// Task
+// Given a number, return the maximum value by rearranging it's digits.
+//
+// Examples:
+//
+// (123) => 321 (786) => 876 ("001") => 100 (999) => 999 (10543) => 54310
+?>
