5748ad8a36633b7d1ef2a321c84cfb0571388466
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-27 03:11:29 -0400

    Add code challenge

---
+++ check_if_flush.php
@@ -0,0 +1,16 @@
+<?php
+// 7 kyu  - Determine if poker hand is flush
+// Determine if the poker hand is flush
+//
+// Your function will be passed a list of strings with 5 strings each representing a poker card in the format "5H" (5 of hearts) meaning the value of the card followed by the initial of its suit (Hearts, Spades, Diamonds or Clubs). No jokers included
+//
+// Your function should return true if the hand is flush meaning if the five cards are of the same suit
+//
+// The possible card values are 2, 3, 4, 5, 6, 7, 8, 9, 10, Jack or Knave (J), Queen (Q), King (K), Ace (A)
+//
+// Eg.
+//
+// AS, 3S, 9S, KS, 4S  should return true
+//
+// AD, 4S, 7H, KS, 10S  should return False.
+?>
