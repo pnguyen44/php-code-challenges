46c82ff153d26c642334b09102e49621d1670ef4
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-27 03:23:10 -0400

    Add rotate_to_max function   - A function that takes an integer and returns the maximum value by   rearranging it's digits.

--- rotate_to_max.php
+++ rotate_to_max.php
@@ -6,4 +6,12 @@
 // Examples:
 //
 // (123) => 321 (786) => 876 ("001") => 100 (999) => 999 (10543) => 54310
+
+function rotate_to_max($n): int {
+  $result = str_split($n);
+  rsort($result);
+  return implode($result);
+}
+$answer = rotate_to_max(786);
+print_r("$answer \n");
 ?>
