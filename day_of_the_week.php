773dd0efe7896846e2653caadda34aa31e64765f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-28 02:12:07 -0400

    Add day_of_the_week function   - A function that takes an date in format d/m/Y(String) and return   what day of the week it was(String).

--- day_of_the_week.php
+++ day_of_the_week.php
@@ -4,4 +4,11 @@
 // Your task is easy, write a function that takes an date in format d/m/Y(String) and return what day of the week it was(String).
 // Example: "21/01/2017" -> "Saturday", "31/03/2017" -> "Friday"
 
+function day_of_the_week($date){
+  $date = str_replace('/','-', $date);
+  return date('l', strtotime($date));
+}
+
+$answer = day_of_the_week("07/12/2000");
+print_r("$answer \n");
 ?>
