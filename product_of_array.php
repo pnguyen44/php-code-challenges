a179154aadfbe3926f14569de05279d8e54ce3c1
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-28 03:02:11 -0400

    Add product function   - A function that returns the product of all elements in an array.

--- product_of_array.php
+++ product_of_array.php
@@ -6,4 +6,13 @@
 // In PHP, if the array is NULL or empty, return NULL.
 //
 // As a challenge, try writing your method in just one line of code. It's possible to have only 36 characters within your method.
+
+function product($a) {
+  return empty($a) ? NULL : array_product($a);
+}
+
+$answer = product([5, 4, 1, 3, 9]);
+print_r("$answer \n");
+
+
 ?>
