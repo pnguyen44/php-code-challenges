dfd8ec0c6e8c12f794aae15471ef342836fd7687
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-28 03:01:33 -0400

    Add code challenge

---
+++ product_of_array.php
@@ -0,0 +1,9 @@
+<?php
+// 7 kyu - Product of Array Items
+//
+// Calculate the product of all elements in an array.
+//
+// In PHP, if the array is NULL or empty, return NULL.
+//
+// As a challenge, try writing your method in just one line of code. It's possible to have only 36 characters within your method.
+?>
