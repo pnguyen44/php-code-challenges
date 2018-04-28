c293f6ec7b9c13150672eb215ec8617303818950
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-28 02:11:30 -0400

    Add code challenge

---
+++ day_of_the_week.php
@@ -0,0 +1,7 @@
+<?php
+// 7 kyu - #~For Kids~# d/m/Y -> Day of the week.
+// #~For Kids Challenges~#
+// Your task is easy, write a function that takes an date in format d/m/Y(String) and return what day of the week it was(String).
+// Example: "21/01/2017" -> "Saturday", "31/03/2017" -> "Friday"
+
+?>
