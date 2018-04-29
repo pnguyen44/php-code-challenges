0dded5c75ab78429cf9f8a7f459deae1427e9da7
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-29 04:59:31 -0400

    Add code challenge

---
+++ nothing_special.php
@@ -0,0 +1,6 @@
+<?php
+// 7 kyu - Nothing special
+// The notorious Captain Schneider has given you a very straight forward mission. Any data that comes through the system make sure that only non-special characters see the light of day.
+//
+// Create a function that given a string, retains only the letters A-Z (upper and lowercase), 0-9 digits, and whitespace characters. Also, returns "Not a string!" if the entry type is not a string.
+?>
