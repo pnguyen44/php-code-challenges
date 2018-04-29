8ccc363a72e79a8aaf311df5e54e4acf2ebc7906
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-29 03:46:47 -0400

    Add code challenge

---
+++ reverse_slice.php
@@ -0,0 +1,10 @@
+<?php
+// 7 kyu - String reverse slicing 101
+// You'll be given a string of characters as an input. Complete the function that returns a list of strings: (a) in the reverse order of the original string, and (b) with each successive string starting one character further in from the end of the original string.
+//
+// Assume the original string is at least 3 characters long. Try to do this using slices and avoid converting the string to a list.
+//
+// Examples
+// '123'   ==>  ['321', '21', '1']
+// 'abcde' ==>  ['edcba', 'dcba', 'cba', 'ba', 'a']
+?>
