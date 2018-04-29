73de0f130c64069454468947ac98a00c765254cb
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-29 05:01:39 -0400

    Add alternative solution

--- nothing_special.php
+++ nothing_special.php
@@ -5,8 +5,13 @@
 // Create a function that given a string, retains only the letters A-Z (upper and lowercase), 0-9 digits, and whitespace characters. Also, returns "Not a string!" if the entry type is not a string.
 
 function nothing_special($s): string {
-return is_string($s) ? preg_replace('/[^[:alnum:][:space:]]/u', '', $s) : 'Not a string!';
+  return is_string($s) ? preg_replace('/[^[:alnum:][:space:]]/u', '', $s) : 'Not a string!';
 }
+
+// Alternative Solution:
+// function nothing_special($s): string {
+//   return !is_string($s) ? "Not a string!" : preg_replace('/[^a-z\d\s]/i', "", $s);
+// }
 
 $answer = nothing_special("St@arboard P^ort$A$ ***little k@$ata fo&r ^my ^trou%bl$esSteamroller2.55");
 print_r("$answer \n");
