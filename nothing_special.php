f66180a51dfd63b935749ada9dc97e40addc8b8b
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-29 05:00:07 -0400

    Add nothing_special function   - A function that given a string, retains only the letters A-Z (upper   and lowercase), 0-9 digits, and whitespace characters. Also, returns   "Not a string!" if the entry type is not a string.

--- nothing_special.php
+++ nothing_special.php
@@ -3,4 +3,12 @@
 // The notorious Captain Schneider has given you a very straight forward mission. Any data that comes through the system make sure that only non-special characters see the light of day.
 //
 // Create a function that given a string, retains only the letters A-Z (upper and lowercase), 0-9 digits, and whitespace characters. Also, returns "Not a string!" if the entry type is not a string.
+
+function nothing_special($s): string {
+return is_string($s) ? preg_replace('/[^[:alnum:][:space:]]/u', '', $s) : 'Not a string!';
+}
+
+$answer = nothing_special("St@arboard P^ort$A$ ***little k@$ata fo&r ^my ^trou%bl$esSteamroller2.55");
+print_r("$answer \n");
+
 ?>
