0559663803c52c29ff26b7c1c8113ab6a6865945
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-04-30 02:57:16 -0400

    Add get_first_python function   - A function that takes an array of objects (associative arrays in   PHP) representing data about developers who have signed up to attend   the next coding meet up that you are organizing. The function should   return the first name and country of the first person that has python   as a language.

--- get_first_python.php
+++ get_first_python.php
@@ -39,4 +39,44 @@
 // Notes:
 //
 // The input array will always be valid and formatted as in the example above.
+
+function get_first_python($a) {
+  foreach($a as $l) {
+    if($l["language"] === "Python") {
+      return $l['first_name']. ', ' .  $l['country'];
+    }
+  }
+  return 'There will be no Python developers';
+}
+
+$list1 = [
+  [
+    "first_name" => "Mark",
+    "last_name" => "G.",
+    "country" => "Scotland",
+    "continent" => "Europe",
+    "age" => 22,
+    "language" => "JavaScript"
+  ],
+  [
+    "first_name" => "Victoria",
+    "last_name" => "T.",
+    "country" => "Puerto Rico",
+    "continent" => "Americas",
+    "age" => 30,
+    "language" => "Python"
+  ],
+  [
+    "first_name" => "Emma",
+    "last_name" => "B.",
+    "country" => "Norway",
+    "continent" => "Europe",
+    "age" => 19,
+    "language" => "Clojure"
+  ]
+];
+
+$answer = get_first_python($list1);
+print_r("$answer \n");
+
 ?>
