93a814c0a217d5988394abd5dd0d9e14fa413345
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-25 17:35:59 -0400

    Add code challenge

---
+++ iccanobif.php
@@ -0,0 +1,8 @@
+<?php
+// 7 kyu - The iccanobiF Sequence
+// Your task is to create an array of size n with the values of the Fibonnaci sequence, and reverse the order in which the sequence is displayed.
+//
+// For example: [1, 1, 2, 3, 5] would become [5, 3, 2, 1, 1]
+//
+// You can assume that n will always be a positive integer between and including, 1 and 64.
+?>
