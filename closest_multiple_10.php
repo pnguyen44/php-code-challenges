a44628e037541a03c1edc4fcfc982655476bb885
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-25 17:20:28 -0400

    Add code challenge

---
+++ closest_multiple_10.php
@@ -0,0 +1,15 @@
+<?php
+// 7 kyu - Return the closest number multiple of 10
+// Given a number return the closest number to it that is divisible by 10.
+//
+// Example input:
+//
+// 22
+// 25
+// 37
+// Expected output:
+//
+// 20
+// 30
+// 40
+?>
