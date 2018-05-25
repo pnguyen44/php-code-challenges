375772a6507ab0cb45ba357f8564833fa56fa3e8
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-25 17:37:26 -0400

    Add iccanobif function    - A function that creates an array of size n with the values of the    Fibonnaci sequence, and reverse the order in which the sequence is    displayed.

--- iccanobif.php
+++ iccanobif.php
@@ -5,4 +5,16 @@
 // For example: [1, 1, 2, 3, 5] would become [5, 3, 2, 1, 1]
 //
 // You can assume that n will always be a positive integer between and including, 1 and 64.
+
+function iccanobif($n) {
+  if ($n === 1) return [1];
+  $arr = [1,1];
+  for($i = 0; $i < $n - 2; $i++){
+    $arr[] = $arr[$i] + $arr[$i + 1];
+  }
+  return array_reverse($arr);
+}
+
+$answer = iccanobif(1);
+print_r("$answer \n");
 ?>
