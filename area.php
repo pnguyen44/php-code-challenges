c9aeab0450609f7b08381f5a34165d595cb8becb
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-30 19:48:06 -0400

    Refactor code

--- area.php
+++ area.php
@@ -3,8 +3,7 @@
 // Find the area of a rectangle when provided with one diagonal and one side of the rectangle. If the input diagonal is less than or equal to the length of the side, return "Not a rectangle". If the resultant area has decimals round it to two places.
 
 function area($d, $l) {
-  if ($d <= $l) return 'Not a rectangle';
-  return round($l * sqrt($d * $d - $l * $l),2);
+  return $d > $l ? round($l * sqrt($d ** 2 - $l ** 2),2) : 'Not a rectangle';
 }
 
 $answer = area(12, 5);
