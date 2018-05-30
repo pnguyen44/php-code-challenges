9312811b2f3198d6b6d362822e1a6a0232ccadaa
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-30 19:43:49 -0400

    Add code challenge

---
+++ area.php
@@ -0,0 +1,5 @@
+<?php
+// 7 kyu - Find the area of the rectangle!
+// Find the area of a rectangle when provided with one diagonal and one side of the rectangle. If the input diagonal is less than or equal to the length of the side, return "Not a rectangle". If the resultant area has decimals round it to two places.
+
+?>
