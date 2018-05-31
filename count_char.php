bbf2bb5a0ff008677153e9c0817d0370fac7575d
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-05-31 19:28:00 -0400

    Add code challenge

---
+++ count_char.php
@@ -0,0 +1,11 @@
+<?php
+// 7 kyu - Count the Characters
+// The goal of this kata is to write a function that takes two inputs: a string and a character. The function will count the number of times that character appears in the string. The count is case insensitive.
+//
+// For example:
+//
+// count_char("fizzbuzz", "z"); // => 4
+// count_char("Fancy fifth fly aloof", "f"); // => 5
+// The character can be any alphanumeric character.
+
+?>
