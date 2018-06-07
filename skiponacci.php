9675a41c833163e0b296fff0a44df4c8a15c0c7c
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-06-06 21:14:37 -0400

    Add code challenge

---
+++ skiponacci.php
@@ -0,0 +1,6 @@
+<?php
+// 7 kyu - The Skiponacci Sequence
+// Your task is to generate the Fibonacci sequence to n places, with each alternating value as "skip". For example:
+//
+// "1 skip 2 skip 5 skip 13 skip 34"
+?>
