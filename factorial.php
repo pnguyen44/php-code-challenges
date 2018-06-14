442a7c2bdd468faf1a9e944a97d84b6cc431aa27
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-06-13 21:13:31 -0400

    Add code challenge

---
+++ factorial.php
@@ -0,0 +1,8 @@
+<?php
+// 7 kyu - Factorial
+//
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+//
+// Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of RangeException (PHP).
+
+?>
