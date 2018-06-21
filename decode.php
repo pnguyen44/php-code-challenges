dad97628a554225c1d3985f7193d08b4d2348a21
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-06-21 01:40:14 -0400

    Add code challenge

---
+++ decode.php
@@ -0,0 +1,13 @@
+<?php
+// 7 kyu - Emily's Eccentric Encoding
+// The encoding replaces all lower case letters in a string with the opposite letter in the alphabet. In other words: a -> z, b -> y, c -> x, d -> w, etc. All other characters remain the same. The two parties have agreed on the encoding and they have generated encoded messages like:
+//
+// "I dlfow orpv gl rmgviervd lm Mlmwzb nlimrmt!"
+// which decodes to:
+//
+// "I would like to interview on Monday morning!"
+// Your job is to write a method called decode that takes an encoded string as input and returns the decoded string as output.
+
+
+
+?>
