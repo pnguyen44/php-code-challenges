e3ff83e6a9554b03b4d2b9150ed7a5a6edc045de
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-06-24 12:32:06 -0400

    Add code challenge

---
+++ battle.php
@@ -0,0 +1,16 @@
+<?php
+// 7 kyu - Battle of the characters (Easy)
+// Description:
+// Groups of characters decided to make a battle. Help them to figure out which group is more powerful. Create a function that will accept 2 variables and return the one who's stronger.
+//
+// Rules:
+// Each character have its own power: A = 1, B = 2, ... Y = 25, Z = 26
+// Only capital chatacters can and will participate a battle.
+// Only two groups to a fight.
+// Group whose total power (A + B + C + ...) is bigger wins.
+// If the powers are equal, it's a tie.
+// Examples:
+//   battle("ONE", "TWO"); // => "TWO"`
+//   battle("ONE", "NEO"); // => "Tie!"
+
+?>
