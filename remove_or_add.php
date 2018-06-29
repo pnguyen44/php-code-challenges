54b3c482891a0978a0427e3373104198600b5ba7
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-06-29 14:47:19 -0400

    Add code challenge

---
+++ remove_or_add.php
@@ -0,0 +1,12 @@
+<?php
+// 7 kyu - Exclamation marks series #9: Remove or add a exclamation mark at the end of words of the sentence
+// Remove or add a exclamation mark at the end of words of the sentence. Words are separated by spaces in the sentence. That is: If a word has one ! at the end, remove it; If a word has no ! at the end, add a ! to the end; If there are more than one ! at the end of word, keep it.
+//
+// Examples
+// removeOrAdd("Hi!") === "Hi"
+// removeOrAdd("Hi! Hi!") === "Hi Hi"
+// removeOrAdd("Hi! Hi") === "Hi Hi!"
+// removeOrAdd("Hi! Hi Hi!!") === "Hi Hi! Hi!!"
+// removeOrAdd("!Hi! !Hi !Hi!!") === "!Hi !Hi! !Hi!!"
+
+?>
