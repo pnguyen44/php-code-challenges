dd2e3a6b43f9e5abbbefd5cd96897db2f792bf51
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-06 17:54:23 -0400

    Add code challenge

---
+++ arr_check.php
@@ -0,0 +1,19 @@
+<?php
+// 7 kyu - Is every value in the array an array?
+// Is every value in the array an array?
+//
+// This should only test the second array dimension of the array. The values of the nested arrays don't have to be arrays.
+//
+// Examples:
+//
+// [[1], [2]] => true
+// ["1", "2"] => false
+// [
+//   new class {
+//     public $one = 1;
+//   },
+//   new class {
+//     public $two = 2;
+//   }
+// ] => false
+?>
