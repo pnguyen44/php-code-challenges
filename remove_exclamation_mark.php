4471183576700947f6a5890c5472ca298ea5089a
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-07 19:45:54 -0400

    Add code challenge

---
+++ remove_exclamation_mark.php
@@ -0,0 +1,13 @@
+<?php
+// 7 kyu - Exclamation marks series #7: Remove words from the sentence if it contains one exclamation mark
+// Remove words from the sentence if it contains one exclamation mark. Words are separated by spaces in the sentence. Please remember, one.
+//
+// Examples
+// remove("Hi!") === ""
+// remove("Hi! Hi!") === ""
+// remove("Hi! Hi! Hi!") === ""
+// remove("Hi Hi! Hi!") === "Hi"
+// remove("Hi! !Hi Hi!") === ""
+// remove("Hi! Hi!! Hi!") === "Hi!!"
+// remove("Hi! !Hi! Hi!") === "!Hi!"
+?>
