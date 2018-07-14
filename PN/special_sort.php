a9084eaa3ca1ea4bfcd924d78ee61293d19cb111
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-14 02:49:19 -0400

    Add special_sort function - A function that takes an array of integers and changes the order of the integers so negative numbers appear first then positive numbers.

--- PN/special_sort.php
+++ PN/special_sort.php
@@ -2,4 +2,17 @@
 // Given an array of integers, write a function that changes the order of the integers so
 // negative numbers appear first then positive numbers. Do not change the order of the integers.  
 
+function special_sort($num) {
+  $neg = array_filter($num, function($x) {
+    return $x < 0;
+  });
+  $pos = array_filter($num, function($x) {
+    return $x >= 0;
+  });
+  return array_merge($neg, $pos);
+}
+
+$test = special_sort([-7, -9, 9, -4, 0, 1]);
+print_r($test);
+
 ?>
