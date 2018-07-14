a4cd42689e3a845d22811091541971b581ffa842
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-14 02:21:47 -0400

    Change code challenge to be case sensitive

--- PN/check_string.php
+++ PN/check_string.php
@@ -1,5 +1,5 @@
 <?php
-// Write a function to determine if a string is a substring of another string.  
+// Write a function to determine if a case sensitive string is a substring of another string.  
 function check_string($str, $word) {
   return stristr($str, $word) ? 'true' : 'false';
 }
