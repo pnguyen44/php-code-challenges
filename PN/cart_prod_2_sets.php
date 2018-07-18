ee016761486c04ddbf75bd0a006cec58a5346785
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-18 04:38:15 -0400

    Add cart_prod_2_sets function   - A function that returns an array of the cartesian product of two   sets.

--- PN/cart_prod_2_sets.php
+++ PN/cart_prod_2_sets.php
@@ -1,5 +1,28 @@
 <?php
 // Write a function that returns an array of the cartesian product of two sets.
 
+function cart_prod_2_sets($setA, $setB) {
+  $result = [];
+  for ($a = 0; $a < count($setA); $a++) {
+    for ($i = 0; $i < count($setB); $i++) {
+      $result[] = [$setA[$a], $setB[$i]];
+    };
+  };
+  return $result;
+}
+
+// test
+$a = [1];
+$b = [1, 2];
+$c = [1, 2, 3];
+$x = ['a'];
+$y = ['a', 'b'];
+$z = ['a', 'b', 'c'];
+
+print_r(cart_prod_2_sets($a,$a));
+print_r(cart_prod_2_sets($a,$x));
+print_r(cart_prod_2_sets($x,$a));
+print_r(cart_prod_2_sets($c,$x));
+print_r(cart_prod_2_sets($b,$y));
 
 ?>
