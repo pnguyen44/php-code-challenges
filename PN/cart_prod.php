1611d2c14f84f9f329e9a8d77056835cdb09a0c8
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-20 03:50:46 -0400

    Clean code

--- PN/cart_prod.php
+++ PN/cart_prod.php
@@ -1,28 +1,22 @@
 <?php
 // Write a function that returns an array of the cartesian product of two sets.
 
-function cart_prod_2_sets($setA, $setB) {
+function cart_prod_2_sets($sets) {
   $result = [];
-  for ($a = 0; $a < count($setA); $a++) {
-    for ($i = 0; $i < count($setB); $i++) {
-      $result[] = [$setA[$a], $setB[$i]];
+  for ($a = 0; $a < count($sets[0]); $a++) {
+    for ($i = 0; $i < count($sets[1]); $i++) {
+      $result[] = [$sets[0][$a], $sets[1][$i]];
     };
   };
   return $result;
 }
 
 // test
-$a = [1];
-$b = [1, 2];
-$c = [1, 2, 3];
-$x = ['a'];
-$y = ['a', 'b'];
-$z = ['a', 'b', 'c'];
+print_r(cart_prod_2_sets([[1], [1]]));
+print_r(cart_prod_2_sets([[1], ['a']]));
+print_r(cart_prod_2_sets([['a'], [1]]));
+print_r(cart_prod_2_sets([[1, 2, 3], ['a']]));
+print_r(cart_prod_2_sets([[1, 2], ['a', 'b']]));
 
-print_r(cart_prod_2_sets($a,$a));
-print_r(cart_prod_2_sets($a,$x));
-print_r(cart_prod_2_sets($x,$a));
-print_r(cart_prod_2_sets($c,$x));
-print_r(cart_prod_2_sets($b,$y));
 
 ?>
