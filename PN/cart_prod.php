2610f596965b2591d1c6dbad3e699ea3e7404c97
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-20 04:45:40 -0400

    Clean code

--- PN/cart_prod.php
+++ PN/cart_prod.php
@@ -30,12 +30,12 @@ function cart_prod_2_sets($sets) {
 function cart_prod_3_sets($sets) {
   $result = [];
   $arr = [];
-  for ($s = 0; $s < count($sets[0]); $s++) {
-    $arr[] = $sets[0][$s];
-    for ($a = 0; $a < count($sets[1]); $a++) {
-      $arr[] = $sets[1][$a];
-      for ($i = 0; $i < count($sets[2]); $i++) {
-        $result[] = array_merge($arr, [$sets[2][$i]]);
+  for ($a = 0; $a < count($sets[0]); $a++) {
+    $arr[] = $sets[0][$a];
+    for ($b = 0; $b < count($sets[1]); $b++) {
+      $arr[] = $sets[1][$b];
+      for ($c = 0; $c < count($sets[2]); $c++) {
+        $result[] = array_merge($arr, [$sets[2][$c]]);
       };
       array_pop($arr);
     };
@@ -46,11 +46,11 @@ function cart_prod_3_sets($sets) {
 
 
 // test
-print_r(cart_prod_2_sets([[1], [1]]));
-print_r(cart_prod_2_sets([[1], ['a']]));
-print_r(cart_prod_2_sets([['a'], [1]]));
-print_r(cart_prod_2_sets([[1, 2, 3], ['a']]));
-print_r(cart_prod_2_sets([[1, 2], ['a', 'b']]));
+// print_r(cart_prod_2_sets([[1], [1]]));
+// print_r(cart_prod_2_sets([[1], ['a']]));
+// print_r(cart_prod_2_sets([['a'], [1]]));
+// print_r(cart_prod_2_sets([[1, 2, 3], ['a']]));
+// print_r(cart_prod_2_sets([[1, 2], ['a', 'b']]));
 print_r(cart_prod_3_sets([[1, 2], [1, 2], [1, 2]]));
 
 ?>
