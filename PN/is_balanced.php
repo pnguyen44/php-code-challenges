9a392cb1aa00821ca4ce7ac0ffeb189221423187
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-25 17:54:16 -0400

    Add is_balanced function - A function that determine if the strings of brackets in the input array are balanced or not based on a set of criteria.

--- PN/is_balanced.php
+++ PN/is_balanced.php
@@ -9,6 +9,32 @@
 // The closing bracket of a pair must be of the same type as the opening bracket, e.g. '( )' is valid, but '[ )' is not valid.
 // Your task is to determine if of the strings of brackets in the input array are valid or invalid by these criteria.
 
+function is_balanced($str) {
+  $brackets = '[]{}()';
+  $open_bracket_stack = [];
+  for ($i = 0; $i < strlen($str); $i++) {
+    $char = $str[$i];
+    $match_open_bracket  = $brackets[strpos($brackets, $char) - 1];
+    if (strpos($brackets, $char) % 2 > 0) {
+      if(count($open_bracket_stack) === 0 || array_pop($open_bracket_stack) !== $match_open_bracket) {
+        return false;
+      };
+    } else {
+      $open_bracket_stack[] = $char;
+    };
+  }
+  return count($open_bracket_stack) === 0;
+}
+
+//test
+// print_r(is_balanced("([)]")); // false
+// print_r(is_balanced("()()")); // true
+// print_r(is_balanced("{}[]()")); // true
+// print_r(is_balanced("{[}]")); // false
+// print_r(is_balanced("{[}]")); // false
+// print_r(is_balanced("[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]")); // true
+// print_r(is_balanced("{}[]()")); // true
+print_r(is_balanced("(())((()())())")); // true
 
 
 
