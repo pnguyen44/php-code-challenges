92edb7bd7a928505ed9c0463a2c4436f25f9b1ef
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-07-25 17:53:20 -0400

    Add code challenge

---
+++ PN/is_balanced.php
@@ -0,0 +1,15 @@
+<?php
+// You are given an array of strings. Each of these strings is made up of bracket characters only :
+// '(', ')', '{',  '}',  '[',  ']'.
+// Programming languages utilize these brackets in balanced pairs, so the following rules are always followed :
+//
+// Every opening bracket has a corresponding closing bracket : '(' with ')', '{' with '}', and '[' with ']'.
+// Two brackets form a pair if they have no open bracket of any type between them.
+// For example: '[}]' is invalid, '[{}]' is valid.
+// The closing bracket of a pair must be of the same type as the opening bracket, e.g. '( )' is valid, but '[ )' is not valid.
+// Your task is to determine if of the strings of brackets in the input array are valid or invalid by these criteria.
+
+
+
+
+?>
