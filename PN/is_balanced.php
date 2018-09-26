12caa53a13012d4b1a20d4a8b8f4631d3a644c0f
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-09-26 11:57:04 -0400

    Add alternative solution

--- PN/is_balanced.php
+++ PN/is_balanced.php
@@ -9,22 +9,42 @@
 // The closing bracket of a pair must be of the same type as the opening bracket, e.g. '( )' is valid, but '[ )' is not valid.
 // Your task is to determine if of the strings of brackets in the input array are valid or invalid by these criteria.
 
+// function is_balanced($str) {
+//   $brackets = '[]{}()';
+//   $open_bracket_stack = [];
+//   for ($i = 0; $i < strlen($str); $i++) {
+//     $char = $str[$i];
+//     $match_open_bracket  = $brackets[strpos($brackets, $char) - 1];
+//     if (strpos($brackets, $char) % 2 > 0) {
+//       if(count($open_bracket_stack) === 0 || array_pop($open_bracket_stack) !== $match_open_bracket) {
+//         return false;
+//       };
+//     } else {
+//       $open_bracket_stack[] = $char;
+//     };
+//   }
+//   return count($open_bracket_stack) === 0;
+// }
+
+// Alternative Solution:
 function is_balanced($str) {
-  $brackets = '[]{}()';
-  $open_bracket_stack = [];
+  $open_bracket_stack= [];
+  $bracketMapping = array('(' => ')', '{' => '}', '[' => ']');
   for ($i = 0; $i < strlen($str); $i++) {
-    $char = $str[$i];
-    $match_open_bracket  = $brackets[strpos($brackets, $char) - 1];
-    if (strpos($brackets, $char) % 2 > 0) {
-      if(count($open_bracket_stack) === 0 || array_pop($open_bracket_stack) !== $match_open_bracket) {
-        return false;
-      };
+    $currentBracket = $str[$i];
+    if (array_key_exists($currentBracket, $bracketMapping)) {
+      $open_bracket_stack[] = $currentBracket;
     } else {
-      $open_bracket_stack[] = $char;
-    };
+      //implies that it is a close bracket
+      if (count($open_bracket_stack) === 0) return "NO";
+      $last_seen_open_bracket = array_pop($open_bracket_stack);
+      $last_seen_open_bracket_matching_bracket = $bracketMapping[$last_seen_open_bracket];
+      if ($currentBracket !== $last_seen_open_bracket_matching_bracket) return 'NO';
+    }
   }
-  return count($open_bracket_stack) === 0;
+  return count($open_bracket_stack) === 0 ? 'YES' : 'NO';
 }
+
 
 //test
 // print_r(is_balanced("([)]")); // false
@@ -34,7 +54,7 @@ function is_balanced($str) {
 // print_r(is_balanced("{[}]")); // false
 // print_r(is_balanced("[{()()}({[]})]({}[({})])((((((()[])){}))[]{{{({({({{{{{{}}}}}})})})}}}))[][][]")); // true
 // print_r(is_balanced("{}[]()")); // true
-print_r(is_balanced("(())((()())())")); // true
+// print_r(is_balanced("(())((()())())")); // true
 
 
 
