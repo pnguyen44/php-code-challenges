f6b60711f2f5fa910d9450438cc6b47b8aa9cd42
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-10-18 19:54:58 -0400

    Add code challenge

---
+++ repeatedString.php
@@ -0,0 +1,13 @@
+<?php
+// Lilah has a string,s, of lowercase English letters that she repeated infinitely many times.
+//
+// Given an integer,n, find and print the number of letter a's in the first n letters of Lilah's infinite string.
+//
+//
+// Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length n in the infinitely repeating string.
+//
+// repeatedString has the following parameter(s):
+//
+// s: a string to repeat
+// n: the number of characters to consider
+?>
