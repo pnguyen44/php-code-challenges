5b255ba8c371d6ff2338705020d01baf9b5d10dd
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-10-19 17:27:16 -0400

    Add bubble sort challenge

---
+++ PN/countSwaps.php
@@ -0,0 +1,24 @@
+<?php
+// Given an array of integers, sort the array in ascending order using the Bubble Sort algorithm above. Once sorted, print the following three lines:
+//
+// 1. Array is sorted in numSwaps swaps., where numSwaps is the number of swaps that took place.
+// 2. First Element: firstElement, where firstElement is the first element in the sorted array.
+// 3. Last Element: lastElement, where lastElement is the last element in the sorted array.
+// Hint: To complete this challenge, you must add a variable that keeps a running tally of all swaps that occur during execution.
+//
+// For example, given a worst-case but small array to sort: a = [6,4,1]  we go through the following steps:
+//
+// swap    a
+// 0       [6,4,1]
+// 1       [4,6,1]
+// 2       [4,1,6]
+// 3       [1,4,6]
+//
+// It took 3 swaps to sort the array. Output would be
+//
+// Array is sorted in 3 swaps.
+// First Element: 1
+// Last Element: 6
+
+
+?>
