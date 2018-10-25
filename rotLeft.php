0d1a39712b272873591894c18d0247b128ac3e57
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-10-24 23:29:13 -0400

    Add code challenge

---
+++ rotLeft.php
@@ -0,0 +1,11 @@
+<?php
+// A left rotation operation on an array shifts each of the array's elements 1 unit to the left. For example, if 2 left rotations are performed on array [1, 2, 3, 4, 5] , then the array would become [3, 4, 5, 1, 2].
+
+// Complete the function rotLeft in the editor below. It should return the resulting array of integers.
+//
+// rotLeft has the following parameter(s):
+//
+// An array of integers a.
+// An integer d, the number of rotations.
+
+?>
