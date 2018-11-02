f99720368853c5fda33b297e5804c9e3508a61b7
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-11-01 21:15:06 -0400

    Update code challenge   - add test cases

--- makeAnagram.php
+++ makeAnagram.php
@@ -1,4 +1,11 @@
 <?php
 // Complete the makeAnagram function in the editor below. It must return an integer representing the minimum total characters that must be deleted to make the strings anagrams.
 
+function makeAnagram($a, $b) {
+
+}
+
+print_r(makeAnagram("cde", "abc")); // 4
+// print_r(makeAnagram("showman", "woman")); // 2
+
 ?>
