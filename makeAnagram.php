fe9645a7aacd1e6ca3bb8ea003f5a031b0363838
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-11-01 21:14:28 -0400

    Add code challenge

---
+++ makeAnagram.php
@@ -0,0 +1,4 @@
+<?php
+// Complete the makeAnagram function in the editor below. It must return an integer representing the minimum total characters that must be deleted to make the strings anagrams.
+
+?>
