058aac608437533dd93246a13f7c7fed5516e54e
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-11-21 20:27:22 -0500

    Add code challenge

---
+++ avery_big_sum.php
@@ -0,0 +1,6 @@
+<?php
+// Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+// Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+
+?>
