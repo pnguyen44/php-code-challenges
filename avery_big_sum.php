b0ea2ca7d7ae9dfd16f0ae7acdd5f59cfd5b7dde
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-11-21 20:28:21 -0500

    Update code challenge

--- avery_big_sum.php
+++ avery_big_sum.php
@@ -2,5 +2,11 @@
 // Calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
 // Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
 
+function aVeryBigSum($ar) {
+
+}
+
+print_r(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005])) //5000000015
+
 
 ?>
