89c7477f8fdf69fc28d3e27c955220cd760fbb2e
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-11-21 20:30:30 -0500

    Add aVeryBigSum function   - A function that calculate and print the sum of the elements in an   array, keeping in mind that some of those integers may be quite large.

--- avery_big_sum.php
+++ avery_big_sum.php
@@ -5,7 +5,7 @@
 // Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
 
 function aVeryBigSum($ar) {
-
+  return array_sum($ar);
 }
 
 print_r(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005])) //5000000015
