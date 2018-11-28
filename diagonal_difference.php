614454588cb4fa7ecb59cefb9a847bac5c4ee4c5
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-11-27 22:24:32 -0500

    Add code challenge

---
+++ diagonal_difference.php
@@ -0,0 +1,21 @@
+<?php
+
+// Diagonal Difference
+
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+//
+// For example, the square matrix arr is shown below:
+//
+// 1 2 3
+// 4 5 6
+// 9 8 9
+//
+// The left-to-right diagonal 1 + 5 + 9 = 15. The right to left diagonal 3 + 5 + 9 = 17. Their absolute difference is | 15 - 17| = 2
+//
+// Complete the diagonalDifference  function in the editor below. It must return an integer representing the absolute diagonal difference.
+//
+// diagonalDifference takes the following parameter:
+//
+// arr: an array of integers .
+
+?>
