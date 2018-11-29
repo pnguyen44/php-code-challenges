e3b81e1ad5e6a1a4fe5ac6fd1e8ba7d67f111bcd
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-11-28 22:19:10 -0500

    Add code challenge

---
+++ plus_minus.php
@@ -0,0 +1,13 @@
+<?php
+// Plus Minus
+// Given an array of integers, calculate the fractions of its elements that are positive, negative, and are zeros. Print the decimal value of each fraction on a new line.
+//
+// For example, given the array arr[1,1,0,-1,-1]  there are 5 elements, two positive, two negative and one zero.
+//
+// Their ratios would be 2/5 = 0.400000 , 2/5  = 0.400000, and 1/5 = 0.200000. It should be printed as
+//
+// 0.400000
+// 0.400000
+// 0.200000
+
+?>
