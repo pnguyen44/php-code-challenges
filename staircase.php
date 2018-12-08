a0a02ee9f1dc73b1adffe938c2c9521a3c99aa99
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-12-07 23:57:31 -0500

    Clean code

--- staircase.php
+++ staircase.php
@@ -12,24 +12,25 @@
 //
 // Write a program that prints a staircase of size n.
 
-function staircase($n) {
-  for($i = 0; $i < $n; $i++) {
-    for ($j = 1; $j <= $n - $i - 1; $j++) {
-      echo " ";
-    };
-
-    for ($k = 1; $k <= $i +1; $k++) {
-      echo "#";
-    };
-    echo "\n";
-  }
-}
+// function staircase($n) {
+//   for($i = 0; $i < $n; $i++) {
+//     for ($j = 1; $j <= $n - $i - 1; $j++) {
+//       echo " ";
+//     };
+//
+//     for ($k = 1; $k <= $i +1; $k++) {
+//       echo "#";
+//     };
+//     echo "\n";
+//   }
+// }
 
 // Alternative Solution:
-// function staircase($n) {
-//   for ($i = 1; $i <= $n; $i++)
-//     echo str_repeat(' ', $n-$i).str_repeat('#', $i), "\n";
-// }
+function staircase($n) {
+  for ($i = 1; $i <= $n; $i++) {
+    echo str_repeat(' ', $n-$i).str_repeat('#', $i), "\n";
+  }
+}
 
 print_r(staircase(6));
 ?>
