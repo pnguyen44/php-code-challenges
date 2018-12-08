e4998e2d218b0b9d59f67bda0eb538e0d0ece5b6
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-12-07 23:50:49 -0500

    Add code challenge

---
+++ staircase.php
@@ -0,0 +1,16 @@
+<?php
+//Staircase
+// Consider a staircase of size n = 4 :
+//
+//    #
+//   ##
+//  ###
+// ####
+//
+//
+// Observe that its base and height are both equal to n, and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+//
+// Write a program that prints a staircase of size n.
+
+
+?>
