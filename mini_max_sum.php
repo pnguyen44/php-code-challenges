a1fb7687acbd31fbcb63106453c2332315cc3091
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-12-08 01:18:45 -0500

    Add mini-max sum code challenge

---
+++ mini_max_sum.php
@@ -0,0 +1,8 @@
+<?php
+// Mini-Max Sum
+
+// Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers.
+// Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+
+?>
