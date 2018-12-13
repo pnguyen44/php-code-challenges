410b3379f1b6d757a6669a8c3e4978dbeed82c6e
pnguyen44 <nguyen.phuongnhat@gmail.com>
2018-12-13 00:07:47 -0500

    Add Birthday Cake Candles code challenge

---
+++ birthday_cake_candles.php
@@ -0,0 +1,18 @@
+<?php
+// Birthday Cake Candles
+
+// You are in charge of the cake for your niece's birthday and have decided the cake will have one candle for each year of her total age.
+// When she blows out the candles, she’ll only be able to blow out the tallest ones.
+// Your task is to find out how many candles she can successfully blow out.
+//
+// For example, if your niece is turning 4 years old, and the cake will have 4 candles of height 4, 4, 1, 3,
+// she will be able to blow out 2 candles successfully, since the tallest candles are of height 4  and there are 2 such candles.
+//
+//
+// Complete the function birthdayCakeCandles in the editor below. It must return an integer representing the number of candles she can blow out.
+//
+// birthdayCakeCandles has the following parameter(s):
+//
+// ar: an array of integers representing candle heights
+
+?>
