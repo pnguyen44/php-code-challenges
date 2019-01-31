dcffd89620ee3d2cd2a140c427844b832de33f21
pnguyen44 <nguyen.phuongnhat@gmail.com>
2019-01-31 04:03:00 -0500

    Add code challenge

---
+++ odd_occurrence_in_array.php
@@ -0,0 +1,15 @@
+<?php
+// A non-empty array A consisting of N integers is given. The array contains an odd number of elements,
+// and each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
+//
+//
+// Write a function:
+//
+// function solution(A);
+//
+// that, given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
+
+
+
+
+?>
